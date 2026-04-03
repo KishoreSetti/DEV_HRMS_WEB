49a8c346fd924cd25b7f3be191c9e1d354a95a13
Karishma0116 <karishma@cortracker360.com>
2026-04-03 21:19:05 +0530

    AttendanceModuleTabs
    AttendanceModuleTabs

--- src/app/features/attendance/wfo-remote-request/wfo-remote-request.component.ts
+++ src/app/features/attendance/wfo-remote-request/wfo-remote-request.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AttendanceService,CreateWorkFromHomeRequest,UpdateWorkFromHomeRequest,BulkApproveRejectWorkFromHome } from '../service/attendance.service';
+import { AttendanceService, CreateWorkFromHomeRequest, UpdateWorkFromHomeRequest, BulkApproveRejectWorkFromHome } from '../service/attendance.service';
 import Swal from 'sweetalert2';
 @Component({
   selector: 'app-wfo-remote-request',
@@ -8,7 +8,7 @@ import Swal from 'sweetalert2';
   styleUrl: './wfo-remote-request.component.css'
 })
 export class WfoRemoteRequestComponent {
- /* ========= SESSION DATA ========= */
+  /* ========= SESSION DATA ========= */
   userId = Number(sessionStorage.getItem('UserId'));
   employeeName = sessionStorage.getItem('Name') || '';
   companyId = Number(sessionStorage.getItem('CompanyId'));
@@ -36,11 +36,14 @@ export class WfoRemoteRequestComponent {
   selectAll = false;
   managerRemarks = '';
 
-  constructor(private attendanceService: AttendanceService) {}
+  selectedTab: string = '';
+
+  constructor(private attendanceService: AttendanceService) { }
 
   ngOnInit(): void {
     this.loadMyRequests();
     this.loadApprovalRequests();
+    this.loadPermissions();
   }
 
   /* ================= SUBMIT REQUEST ================= */
@@ -49,7 +52,7 @@ export class WfoRemoteRequestComponent {
     debugger;
     this.attendanceService.createRequest(this.wfhForm).subscribe({
       next: () => {
-        Swal.fire('Created','WFH request submitted successfully','success');
+        Swal.fire('Created', 'WFH request submitted successfully', 'success');
         this.resetForm();
         this.loadMyRequests();
       },
@@ -119,11 +122,40 @@ export class WfoRemoteRequestComponent {
 
     this.attendanceService.bulkApproveReject(payload).subscribe({
       next: () => {
-        Swal.fire("Approved",`${status} successfully`,'success');
+        Swal.fire("Approved", `${status} successfully`, 'success');
         this.managerRemarks = '';
         this.loadApprovalRequests();
       },
       error: err => console.error(err)
     });
+  }
+  canViewPersonal = false;
+  canViewManager = false;
+  loadPermissions() {
+    const menus = JSON.parse(sessionStorage.getItem("Menus") || "[]");
+
+    const personal = menus.find(
+      (m: any) => m.menuName?.trim().toLowerCase() === "my request"
+    );
+
+    const managerapproval = menus.find((m: any) =>
+      m.menuName?.trim().toLowerCase() === "manager approval"
+    );
+
+
+    this.canViewPersonal = personal?.canView ?? false;
+    this.canViewManager = managerapproval?.canView ?? false;
+
+
+    if (this.canViewPersonal) this.selectedTab = 'tab1';
+    else if (this.canViewManager) this.selectedTab = 'tab2';
+
+    console.log("Menus:", menus);
+    console.log("Manager Approval:", managerapproval);
+
+    menus.forEach((m: any) => {
+      console.log("Menu Name:", m.menuName);
+    });
+
   }
 }
