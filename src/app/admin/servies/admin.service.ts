d59bbffd80b5457b19a6667f8b45fc41ea3bf8fa
praveennani360 <praveen@cortracker360.com>
2026-04-08 12:19:36 +0530

    Visa type praveen

--- src/app/admin/servies/admin.service.ts
+++ src/app/admin/servies/admin.service.ts
@@ -1534,8 +1534,10 @@ CreateEmployeeImmigration(formData: FormData): Observable<any> {
     return this.http.delete(`${this.baseUrl}/Employee/DeleteImmigration/${id}`)
   }
 // Visa Types Dropdown
-getVisaTypes(): Observable<any[]> {
-  return this.http.get<any[]>(`${this.baseUrl}/Employee/GetVisaTypes`);
+getVisaTypes(companyId: number, regionId: number): Observable<any[]> {
+  return this.http.get<any[]>(
+    `${this.baseUrl}/Employee/GetVisaTypes?companyId=${companyId}&regionId=${regionId}`
+  );
 }
 
 // Status Dropdown
@@ -2669,6 +2671,35 @@ getEmploymentTypesByFilter(companyId: number, regionId: number) {
   debugger;
   return this.http.get(
     `${this.baseUrl}/MasterData/employment-type/filter?companyId=${companyId}&regionId=${regionId}`
+  );
+}
+
+getVisaTypeList(userId: number) {
+  return this.http.get(
+    `${this.baseUrl}/MasterData/visatype-list/${userId}`
+  );
+}
+
+createVisaType(data: any) {
+  debugger;
+  return this.http.post(
+    `${this.baseUrl}/MasterData/CreateVisaType`,
+    data
+  );
+}
+
+updateVisaType(data: any) {
+  debugger;
+  return this.http.post(
+    `${this.baseUrl}/MasterData/UpdateVisaType`,
+    data
+  );
+}
+
+deleteVisaType(id: number) {
+  return this.http.post(
+    `${this.baseUrl}/MasterData/DeleteVisaType?id=${id}`,
+    {}
   );
 }
 }
