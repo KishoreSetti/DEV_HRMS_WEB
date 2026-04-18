1582a09108dd3c9ac174dce109e984b78a27a2c9
Karishma0116 <karishma@cortracker360.com>
2026-04-18 10:05:38 +0530

    EmployeeLetters
    EmployeeLetters

--- src/app/admin/servies/admin.service.ts
+++ src/app/admin/servies/admin.service.ts
@@ -1691,6 +1691,11 @@ updateEmployeeLetter(id: number, formData: FormData): Observable<any> {
 deleteEmployeeLetter(id: number): Observable<any> {
   return this.http.post(`${this.baseUrl}/employee/deleteletters?id=${id}`, {});
 }
+getMyLetters(employeeCode: string) {
+  return this.http.get<any[]>(
+    `${this.baseUrl}/employee/GetMyLetters/${employeeCode}`
+  );
+}
 
 // -------------------------------------------------------------
 // 🔹 EMPLOYEE  Forms  OPERATIONS
