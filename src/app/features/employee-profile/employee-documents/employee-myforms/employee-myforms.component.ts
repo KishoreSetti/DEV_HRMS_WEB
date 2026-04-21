2edb8f3aff3abfc7870dac75b461f821c39ab369
Karishma0116 <karishma@cortracker360.com>
2026-04-21 17:34:16 +0530

    Employee_MyForms
    Employee_MyForms

---
+++ src/app/features/employee-profile/employee-documents/employee-myforms/employee-myforms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-myforms',
+  standalone: false,
+  templateUrl: './employee-myforms.component.html',
+  styleUrl: './employee-myforms.component.css'
+})
+export class EmployeeMyformsComponent {
+
+}
