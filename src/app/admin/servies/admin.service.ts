32335a7806d545a765a3be27e6395ef2b0186e5e
Karishma0116 <karishma@cortracker360.com>
2026-04-24 15:35:24 +0530

    EmployeeForm_2
    EmployeeForm_2

--- src/app/admin/servies/admin.service.ts
+++ src/app/admin/servies/admin.service.ts
@@ -1723,6 +1723,20 @@ updateEmployeeForms(id: number, formData: FormData): Observable<any> {
 deleteEmployeeForms(id: number): Observable<any> {
   return this.http.post(`${this.baseUrl}/employee/DeleteForm?id=${id}`, {});
 }
+getMyForms(employeeCode: string) {
+  return this.http.get<any[]>(
+    `${this.baseUrl}/employee/GetMyForms/${employeeCode}`
+  );
+}
+uploadEmployeeFiles(formData: FormData) {
+  return this.http.post(
+    `${this.baseUrl}/employee/UploadEmployeeFiles`,
+    formData
+  );
+}
+updateFormStatus(data: any) {
+  return this.http.post(`${environment.apiUrl}/EmployeeForms/UpdateStatus`, data);
+}
 // -------------------------------------------------------------
 // 🔹 EMPLOYEE  Document  OPERATIONS
 // -------------------------------------------------------------
